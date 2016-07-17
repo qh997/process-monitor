19cc0edb6a987a5296bc054355db8f473024edf3
Geng Shuang <qh997@hotmail.com>
2016-07-18 00:29:38 +0800

    Fix SYSTEM col error

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEFAULT_INTERVAL=30
-FS_RECORD='filesystem.log'
+FS_RECORD='filesystem.csv'
 PAGE_SIZE_K=$(($(getconf PAGE_SIZE) / 0x400))
 WORKDIR=$(cd `dirname "$0"`; pwd)
 declare -a PROCESS=('pro1' 'pro 2')
@@ -91,25 +91,54 @@ function system_process_monitor {
 	local _data="${current_date},${current_second},\"${_pro_name}\""
 
 	if [ "$_pro_name" == "SYSTEM" ]; then
-		if [ ! -r "${OUTDIR}/${_pro_name}.log" ]; then
-			echo 'Date,Time,Porcess,cpu,mem_total,mem_used,swap_total,swap_used' \
-				> "${OUTDIR}/${_pro_name}.log"
+		if [ ! -r "${OUTDIR}/${_pro_name}.csv" ]; then
+			echo 'Date,Time,Porcess,CPU,MemTotal,MemAvailable,SwapTotal,SwapCached' \
+				> "${OUTDIR}/${_pro_name}.csv"
 		fi
 
+		_data="${_data},"
 		_data="${_data},$(cat /proc/meminfo | awk '$1 == "MemTotal:"{print $2}')"
 		_data="${_data},$(cat /proc/meminfo | awk '$1 == "MemAvailable:"{print $2}')"
 		_data="${_data},$(cat /proc/meminfo | awk '$1 == "SwapTotal:"{print $2}')"
 		_data="${_data},$(cat /proc/meminfo | awk '$1 == "SwapCached:"{print $2}')"
 
-		echo ${_data} >> "${OUTDIR}/${_pro_name}.log"
+		echo ${_data} >> "${OUTDIR}/${_pro_name}.csv"
 	else
-		if [ ! -r "${OUTDIR}/${_pro_name}.log" ]; then
+		if [ ! -r "${OUTDIR}/${_pro_name}.csv" ]; then
 			echo 'Date,Time,Process,Args,PID,CPU%,Mem%,Mem(RSS in kB),VMem(VSZ in kB),SWAP(kB)' \
-				> "${OUTDIR}/${_pro_name}.log"
+				> "${OUTDIR}/${_pro_name}.csv"
 		fi
 
 		process_monitor "${_pro_name}" "${_data}"
 	fi
+}
+
+function system_pcpu {
+	CPULOG_1=$(cat '/proc/stat' | awk '$1 == "cpu"{print $2" "$3" "$4" "$5" "$6" "$7" "$8}')
+	echo CPULOG_1 = $CPULOG_1
+	echo CPULOG_0 = $CPULOG_0
+	SYS_IDLE_1=$(echo "$CPULOG_1" | awk '{print $4}')
+	echo SYS_IDLE_1 = $SYS_IDLE_1
+	echo SYS_IDLE_0 = $SYS_IDLE_0
+	Total_1=$(echo "$CPULOG_1" | awk '{print $1+$2+$3+$4+$5+$6+$7}')
+	echo Total_1 = $Total_1
+	echo Total_0 = $Total_0
+
+	SYS_IDLE=$(($SYS_IDLE_1 - $SYS_IDLE_0))
+	echo SYS_IDLE = $SYS_IDLE
+	Total=$(($Total_1 - $Total_0))
+	echo Total = $Total
+	SYS_USAGE=$(($SYS_IDLE / $Total * 100))
+	echo SYS_USAGE = $SYS_USAGE
+	SYS_Rate=$((100 - $SYS_USAGE))
+	echo SYS_Rate = $SYS_Rate
+	Disp_SYS_Rate=`expr "scale=2; $SYS_Rate/1" |bc`
+	echo Disp_SYS_Rate = $Disp_SYS_Rate
+	echo $Disp_SYS_Rate%
+
+	CPULOG_0=${CPULOG_1}
+	SYS_IDLE_0=${SYS_IDLE_1}
+	Total_0=${Total_1}
 }
 
 function process_monitor {
@@ -130,19 +159,24 @@ function process_monitor {
 			__data="${__data},$(ps -p ${_pid} -o 'vsz' | sed -e '1d' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 			__data="${__data},$(awk '/Swap:/{a=a+$2}END{print a}' /proc/${_pid}/smaps)"
 
-			echo "${__data}" >> "${OUTDIR}/${_pro_name}.log"
+			echo "${__data}" >> "${OUTDIR}/${_pro_name}.csv"
 		done
 	else
-		echo "${_data},,<NO SUCH PROCESS>,,,,,," >> "${OUTDIR}/${_pro_name}.log"
+		echo "${_data},,<NO SUCH PROCESS>,,,,,," >> "${OUTDIR}/${_pro_name}.csv"
 	fi
 }
 
 init $@
 
+# CPULOG_0=$(cat '/proc/stat' | awk '$1 == "cpu"{print $2" "$3" "$4" "$5" "$6" "$7" "$8}')
+# SYS_IDLE_0=$(echo $CPULOG_0 | awk '{print $4}')
+# Total_0=$(echo $CPULOG_0 | awk '{print $1+$2+$3+$4+$5+$6+$7}')
+# sleep 1
+
 last_checked_time=0
 checked=0
 while [ 1 ]; do
-	current_date=$(date +"%Y-%m-%d.%H:%M:%S")
+	current_date=$(date +"%Y-%m-%d %H:%M:%S")
 	current_second=$(date +"%s")
 	#current_second=$(date +"%s.%N" | sed 's/\(\.[0-9][0-9][0-9]\).*/\1/')
 
